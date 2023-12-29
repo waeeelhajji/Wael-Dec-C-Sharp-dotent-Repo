d78831c928011f68b2917ca8bd67c19b6dc0f475
waeeelhajji <waelhajji.contact@gmail.com>
2023-12-29 08:14:02 +0100

    Week1day1

---
+++ Week1/Day1/FirstProject/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//! DataTypes
+
+// Javascript
+
+// var name = "hello";
+
+// == or ===
+
+// Python
+
+// name = "hello"
+
+// statically typed Language
+// You have a  to specify the type of variables or function you want to declare or Create
+
+
+string name = "Wael";
+int number = 7;
+double dec = 3.14;
+float floatvalue = 1.2f;
+bool isTired = true;
+
+//!-------------------
+// Lists and Dictionaries
+// Arrays and List
+// Arrays are fixed length
+string[] groceryList = new string[4];
+// [null,null,null,null]
+string[] groceryList2 = { "Carrots", "Turkey", "Bread", "Milk" };
+//-------------------------0          1         2          3
+
+
+groceryList2[2] = "Ham";
+groceryList[2] = "Ham";
+// [null,null,"Ham",null]
+Console.WriteLine(groceryList2[1]);
+Console.WriteLine(groceryList2.Length);
+
+
+// Lists are variable length
+
+List<int> numberList = new List<int>();
+// add to the array
+numberList.Add(3);
+numberList.Add(4);
+numberList.Add(5);
+numberList.Add(6);
+// this removes the value of 3
+numberList.Remove(5);
+// Remove at the location of index
+// numberList.RemoveAt(3);
+//
+numberList.Insert(1, 77);
+Console.WriteLine(numberList);
+foreach (int i in numberList)
+{
+    Console.WriteLine(i);
+
+}
+// Fun Fact about C#
+
+// string contains multiple letters
+string doblequotation = "Hello";
+
+// char uses  or contain one character
+// uses one Quotation
+char singleChar = 'd';
+
+// Functions
+
+static void HelloCSharp()
+{
+    Console.WriteLine("Welcome to C#");
+
+}
+HelloCSharp();
+
+// ----------------parameters in C#
+static int DoMatch(int x, int y)
+{
+    return x + y;
+}
+
+Console.WriteLine(DoMatch(13, 17));
+
+int answer = DoMatch(10, 77);
+
+Console.WriteLine(answer);
---
+++ Week1/Day1/Week1Day1.md
@@ -0,0 +1,34 @@
+# Week 1 Day 1
+
+## Objectives
+
+* Download dotnet 6 [link](https://dotnet.microsoft.com/en-us/download/dotnet/6.0)
+* Run `dotnet --version` to verify you have dotnet version 6 on your computer
+* Make sure MySQL is running a-okay on your computer
+* Create your first project using `dotnet new console -o ProjectName`
+* Run your first project with `dotnet run`
+* Become familiar with C# syntax
+* Test your knowledge of the differences between JavaScript and C# by completing the Basic 13 in JavaScript and converting it to C#
+
+## Special notes
+
+* Use Pascal Case (PascalCase) for your naming conventions
+* Curly braces each get their own line
+
+```
+    static void MyFunction()
+    {
+        // Your code here
+    }
+    
+    if(condition)
+    {
+        // Your code
+    } 
+    else 
+    {
+        // Your code
+    }
+```
+
+* Study up on the difference between an Array and a List in C#. We will primarily use Lists going forward but it's important to know there is a difference!
